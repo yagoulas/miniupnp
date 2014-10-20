432ec1aa9f1c9dedc0b294c870a87ffea831347f
Giannis Adamopoulos <johnyadams@outlook.com>
2014-10-20 11:59:00 +0300

    Support msvc build right in setup.py

--- miniupnpc/setup.py
+++ miniupnpc/setup.py
@@ -8,6 +8,32 @@
 # replace libminiupnpc.a by libminiupnpc.so for shared library usage
 from distutils.core import setup, Extension
 from distutils import sysconfig
+
+import os
+import subprocess
+import sys
+
+# build static lib if not exists
+prj_root = os.path.dirname(__file__)
+if sys.platform != "win32":
+    lib = "libminiupnpc.a"
+
+    if not os.path.exists(os.path.join(prj_root, "libminiupnpc.a")):
+        subprocess.call(['make', 'upnpc-static'])
+else:
+    lib = "miniupnpc.lib"
+    if not os.path.exists(os.path.join(prj_root, "msvc\\release\\miniupnpc.lib")):
+        vcbuild = os.path.expandvars("%VS90COMNTOOLS%\\..\\..\\vc\\vcpackages\\vcbuild.exe")
+
+        os.chdir("msvc")
+        subprocess.call([vcbuild, 'miniupnpc.vcproj'])
+        os.chdir("..")
+
+if sys.platform in ("win32", "cygwin"):
+    libraries=["ws2_32", "iphlpapi"]
+else:
+    libraries=[]
+
 sysconfig.get_config_vars()["OPT"] = ''
 sysconfig.get_config_vars()["CFLAGS"] = ''
 setup(name="miniupnpc",
@@ -19,6 +45,6 @@ setup(name="miniupnpc",
       description='miniUPnP client',
       ext_modules=[
          Extension(name="miniupnpc", sources=["miniupnpcmodule.c"],
-                   extra_objects=["libminiupnpc.a"])
+                   extra_objects=[lib], libraries=libraries)
       ])
 
